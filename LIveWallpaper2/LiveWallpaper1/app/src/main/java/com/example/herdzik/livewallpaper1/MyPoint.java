10e2a6a1c6140d0a1ce5a6da623590d20d96070e
Alex Herdzik <ajh2510@rit.edu>
2015-08-26 22:02:27 -0700

    addded circly one thats broken a little

---
+++ LIveWallpaper2/LiveWallpaper1/app/src/main/java/com/example/herdzik/livewallpaper1/MyPoint.java
@@ -0,0 +1,16 @@
+package com.example.herdzik.livewallpaper1;
+
+/**
+ * Created by Alex on 8/26/2015.
+ */
+public class MyPoint {
+    String text;
+    public int x;
+    public int y;
+
+    public MyPoint(String text, int x, int y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ LIveWallpaper2/LiveWallpaper1/app/src/main/java/com/example/herdzik/livewallpaper1/SetWallpaperActivity.java
@@ -0,0 +1,22 @@
+package com.example.herdzik.livewallpaper1;
+import android.app.Activity;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SetWallpaperActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_activity_layout);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                new ComponentName(this, MyWallpaperService.class));
+        startActivity(intent);
+    }
+}
